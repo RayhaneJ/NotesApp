2933e5f594681b8b3829ff242b35c96ed0cf4351
rayhaneJ <rayhane.jebbari@3ds.com>
2020-10-25 00:06:11 +0200

    vernote

---
+++ NotesApp/View/NotesWindow.xaml.cs
@@ -0,0 +1,206 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using NotesApp.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Speech.Recognition;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NotesApp.View
+{
+    /// <summary>
+    /// Interaction logic for NotesWindow.xaml
+    /// </summary>
+    public partial class NotesWindow : Window
+    {
+        SpeechRecognitionEngine recognizer;
+        NotesVM notesVM;
+        public NotesWindow()
+        {
+            InitializeComponent();
+
+            notesVM = new NotesVM();
+            container.DataContext = notesVM;
+            notesVM.SelectedNoteChanged += NotesVM_SelectedNoteChanged;
+
+            var currentCulture = (from r in SpeechRecognitionEngine.InstalledRecognizers()
+                                 where r.Culture.Equals(Thread.CurrentThread.CurrentCulture)
+                                 select r).FirstOrDefault();
+            recognizer = new SpeechRecognitionEngine(currentCulture);
+
+            GrammarBuilder builder = new GrammarBuilder();
+            builder.AppendDictation();
+            Grammar grammaer = new Grammar(builder);
+
+            recognizer.LoadGrammar(grammaer);
+            recognizer.SetInputToDefaultAudioDevice();
+            recognizer.SpeechRecognized += Recognizer_SpeechRecognized;
+
+            var fontFamilies = Fonts.SystemFontFamilies.OrderBy(f => f.Source);
+            fontFamilyComboBox.ItemsSource = fontFamilies;
+
+            List<double> fontSizes = new List<double>() { 8, 9, 10, 11, 12, 14, 16, 28, 48, 72 };
+            fontSizeComboBox.ItemsSource = fontSizes;
+        }
+
+        private void NotesVM_SelectedNoteChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(notesVM.SelectedNote.FileLocation))
+            {
+                using (FileStream fileStream = new FileStream(notesVM.SelectedNote.FileLocation, FileMode.Open))
+                {
+                    TextRange range = new TextRange(contentRichTextBox.Document.ContentStart, contentRichTextBox.Document.ContentEnd);
+                    range.Load(fileStream, DataFormats.Rtf);
+                }
+            }
+        }
+
+        protected override void OnActivated(EventArgs e)
+        {
+            base.OnActivated(e);
+
+            if (string.IsNullOrEmpty(App.UserId))
+            {
+                LoginWindow loginWindow = new LoginWindow();
+                loginWindow.ShowDialog();
+            }
+        }
+
+        private void Recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string recognizedText = e.Result.Text;
+
+            contentRichTextBox.Document.Blocks.Add(new Paragraph(new Run(recognizedText)));
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        
+        private void SpeechButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool isButtonEnabled = (sender as ToggleButton).IsChecked ?? false;
+            if (isButtonEnabled)
+            {
+                recognizer.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            else
+            {
+                recognizer.RecognizeAsyncStop();
+            }
+        }
+
+        private void ContentRichTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int ammountCharacters = (new TextRange(contentRichTextBox.Document.ContentStart, contentRichTextBox.Document.ContentEnd)).Text.Length;
+
+            statusTextBlock.Text = $"Document length: {ammountCharacters} characters";
+        }
+
+        private void BoldButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool isButtonEnabled = (sender as ToggleButton).IsChecked ?? false;
+
+            if (isButtonEnabled)
+                contentRichTextBox.Selection.ApplyPropertyValue(Inline.FontWeightProperty, FontWeights.Bold);
+            else
+                contentRichTextBox.Selection.ApplyPropertyValue(Inline.FontWeightProperty, FontWeights.Normal);
+        }
+
+        private void ContentRichTextBox_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            var selectedWeight = contentRichTextBox.Selection.GetPropertyValue(Inline.FontWeightProperty);
+            boldButton.IsChecked = (selectedWeight != DependencyProperty.UnsetValue) && (selectedWeight.Equals(FontWeights.Bold));
+
+            var selectedStyle = contentRichTextBox.Selection.GetPropertyValue(Inline.FontStyleProperty);
+            italicButton.IsChecked = (selectedStyle != DependencyProperty.UnsetValue) && (selectedStyle.Equals(FontStyles.Italic));
+
+            var selecteDecoration = contentRichTextBox.Selection.GetPropertyValue(Inline.TextDecorationsProperty);
+            underlineButton.IsChecked = (selecteDecoration != DependencyProperty.UnsetValue) && (selecteDecoration.Equals(TextDecorations.Underline));
+
+            fontFamilyComboBox.SelectedItem = contentRichTextBox.Selection.GetPropertyValue(Inline.FontFamilyProperty);
+            fontSizeComboBox.Text = (contentRichTextBox.Selection.GetPropertyValue(Inline.FontSizeProperty)).ToString();
+        }
+
+        private void ItalicButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool isButtonEnabled = (sender as ToggleButton).IsChecked ?? false;
+
+            if (isButtonEnabled)
+                contentRichTextBox.Selection.ApplyPropertyValue(Inline.FontStyleProperty, FontStyles.Italic);
+            else
+                contentRichTextBox.Selection.ApplyPropertyValue(Inline.FontStyleProperty, FontStyles.Normal);
+        }
+
+        private void UnderlineButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool isButtonEnabled = (sender as ToggleButton).IsChecked ?? false;
+
+            if (isButtonEnabled)
+                contentRichTextBox.Selection.ApplyPropertyValue(Inline.TextDecorationsProperty, TextDecorations.Underline);
+            else
+            {
+                TextDecorationCollection textDecorations;
+                (contentRichTextBox.Selection.GetPropertyValue(Inline.TextDecorationsProperty) as TextDecorationCollection).TryRemove(TextDecorations.Underline, out textDecorations);
+                contentRichTextBox.Selection.ApplyPropertyValue(Inline.TextDecorationsProperty, textDecorations);
+            }
+        }
+
+        private void FontFamilyComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(fontFamilyComboBox.SelectedItem != null)
+            {
+                contentRichTextBox.Selection.ApplyPropertyValue(Inline.FontFamilyProperty, fontFamilyComboBox.SelectedItem);
+            }
+        }
+
+        private void FontSizeComboBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            contentRichTextBox.Selection.ApplyPropertyValue(Inline.FontSizeProperty, fontSizeComboBox.Text);
+        }
+
+        private void SaveFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            string fileName = $"{notesVM.SelectedNote.Id}";
+            string rtfFile = System.IO.Path.Combine(Environment.CurrentDirectory, fileName);
+            notesVM.SelectedNote.FileLocation = rtfFile;
+
+            using (FileStream fileStream = new FileStream(rtfFile, FileMode.Create))
+            {
+                TextRange textRange = new TextRange(contentRichTextBox.Document.ContentStart, contentRichTextBox.Document.ContentEnd);
+                textRange.Save(fileStream, DataFormats.Rtf);
+            }
+
+            UploadFile(rtfFile, fileName);
+
+            notesVM.UpdateSelectedNote();
+        }
+
+        private async void UploadFile(string rtfFileLocation, string fileName)
+        {
+            var account = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=mynotestorage;AccountKey=KCBbpoBV1PzGWDa21tANjiitbyL4VNu6CpVt/AdgRATrqCC429yonNsaCpfA97QBuNi781PLiITexQ7NG54Fcw==;EndpointSuffix=core.windows.net");
+            var client = account.CreateCloudBlobClient();
+            var container = client.GetContainerReference("notes");
+            await container.CreateIfNotExistsAsync();
+            var blob = container.GetBlockBlobReference(fileName);
+
+            using(FileStream fileStream = new FileStream(rtfFileLocation, FileMode.Open))
+            {
+                await blob.UploadFromStreamAsync(fileStream);
+            }
+        }
+    }
+}
---
+++ NotesApp/ViewModel/NotesVM.cs
@@ -0,0 +1,236 @@
+﻿using NotesApp.Model;
+using NotesApp.ViewModel.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesApp.ViewModel
+{
+    public class NotesVM : INotifyPropertyChanged
+    {
+        private bool isEditing;
+
+        public bool IsEditing
+        {
+            get { return isEditing; }
+            set
+            {
+                isEditing = value;
+                OnPropertyChanged("IsEditing");
+            }
+        }
+
+        public ObservableCollection<Notebook> Notebooks { get; set; }
+
+        private Notebook selectedNotebook;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public event EventHandler SelectedNoteChanged;
+
+        public Notebook SelectedNotebook
+        {
+            get { return selectedNotebook; }
+            set
+            {
+                selectedNotebook = value;
+                ReadNotes();
+                OnPropertyChanged("SelectedNotebook");
+            }
+        }
+
+        private Note note;
+                
+        public Note SelectedNote
+        {
+            get { return note; }
+            set 
+            { 
+                note = value;
+                SelectedNoteChanged(this, new EventArgs());
+                OnPropertyChanged("SelectedNote");
+            }
+        }
+
+        public ObservableCollection<Note> Notes { get; set; }
+        public NewNotebookCommand NewNotebookCommand { get; set; }
+        public NewNoteCommand NewNoteCommand { get; set; }
+        public BeginEditCommand BeginEditCommand { get; set; }
+        public HasEditedCommand HasEditedCommand { get; set; }
+
+        public NotesVM()
+        {
+            IsEditing = false;
+
+            NewNotebookCommand = new NewNotebookCommand(this);
+            NewNoteCommand = new NewNoteCommand(this);
+            BeginEditCommand = new BeginEditCommand(this);
+            HasEditedCommand = new HasEditedCommand(this);
+
+            Notebooks = new ObservableCollection<Notebook>();
+            Notes = new ObservableCollection<Note>();
+
+            ReadNotebooks();
+            ReadNotes();
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public async void CreateNotebook()
+        {
+            Notebook newNotebook = new Notebook()
+            {
+                Name = "New notebook",
+                UserId = App.UserId
+            };
+
+            //DatabaseHelper.Insert(newNotebook);
+
+            try
+            {
+                await App.mobileServiceClient.GetTable<Notebook>().InsertAsync(newNotebook);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            ReadNotebooks();
+        }
+
+        public async void CreateNote(string notebookId)
+        {
+            Note newNote = new Note()
+            {
+                NotebookId = notebookId,
+                CreatedTime = DateTime.Now,
+                UpdatedTime = DateTime.Now,
+                Title = "New note"
+            };
+
+            //DatabaseHelper.Insert(newNote);
+
+            try
+            {
+                await App.mobileServiceClient.GetTable<Note>().InsertAsync(newNote);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            ReadNotes();
+        }
+
+        public async void ReadNotebooks()
+        {
+            //using (SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(DatabaseHelper.dbFile))
+            //{
+            //    var notebooks = conn.Table<Notebook>().ToList();
+
+            //    Notebooks.Clear();
+            //    foreach(var notebook in notebooks)
+            //    {
+            //        Notebooks.Add(notebook);
+            //    }
+            //}
+
+            try
+            {
+                var notebooks = await App.mobileServiceClient.GetTable<Notebook>()
+                    .Where(u => u.Id == App.UserId).ToListAsync();
+
+                Notebooks.Clear();
+
+                notebooks.ForEach((notebook) =>
+                {
+                    Notebooks.Add(notebook);
+                });
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async void ReadNotes()
+        {
+            //using (SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(DatabaseHelper.dbFile))
+            //{
+            //    if (SelectedNotebook != null)
+            //    {
+            //        var notes = conn.Table<Note>().Where(n => n.NotebookId == SelectedNotebook.Id).ToList();
+
+            //        Notes.Clear();
+            //        foreach(var note in notes)
+            //        {
+            //            Notes.Add(note);
+            //        }
+            //    }
+            //}
+
+            var notes = await App.mobileServiceClient.GetTable<Note>()
+                    .Where(u => u.NotebookId == SelectedNotebook.Id).ToListAsync();
+
+            Notes.Clear();
+
+            notes.ForEach((note) =>
+            {
+                Notes.Add(note);
+            });
+        }
+
+        public void StartEditing()
+        {
+            IsEditing = true;
+        }
+
+        public async void HasRenamed(Notebook notebook)
+        {
+            if(notebook != null)
+            {
+                //DatabaseHelper.Update(notebook);
+
+                try
+                {
+                    await App.mobileServiceClient.GetTable<Notebook>().UpdateAsync(SelectedNotebook);
+                    IsEditing = false;
+                    ReadNotebooks();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+
+                
+            }
+        }
+
+        public async void UpdateSelectedNote()
+        {
+            //DatabaseHelper.Update(SelectedNote);
+
+            try
+            {
+                await App.mobileServiceClient.GetTable<Note>().UpdateAsync(SelectedNote);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
